c33c9d6f8530c660a589ff5b638bec43b022a740
NK85 <38337158+NK85@users.noreply.github.com>
2018-04-13 00:59:40 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,155 @@
+var wait = 0;
+var heroes;
+var autobuy = 1;
+var autoabil = 1;
+var autoreborn = 1;
+var rebornlvl = 45;
+var abilscreen = 0;
+var lvllimit = ParseGold("100B");
+
+
+function Random(min,max)
+{
+  return Math.floor((Math.random() * (max-min+1))+min);
+}
+
 function main()
 {
+  if(abilscreen)
+  {
+    var abils = document.getElementsByClassName("abil locked");
+    for(i = 0;i < abils.length;i++)
+    {
+      if(CheckAbil(abils[i]))
+      {
+        setTimeout(AbilBuy,Random(300,600),abils[i]);
+        break;
+      }
+    }
+    if(i == abils.length)
+    {
+      abilscreen = 0;
+      document.getElementsByClassName("btn-close-x")[0].click();
+    }
+    return;
+  }
+  heroes = document.getElementsByClassName("hero-card");
+  var i;
+  for(i = 0; i < heroes.length; i++)
+  {
+    if(heroes[i].childNodes.length < 6)
+    {
+      continue;
+    }
+    break;
+  }
+  if(autobuy)
+  {
+    if(i == heroes.length) 
+    {
+      if(CheckBuy(1,heroes[i]))
+      {setTimeout(HeroUnlock,Random(300,600),heroes[i]);}
+    }
+    else
+    {
+      if(GetHeroLevel(heroes[i]) < 10)
+      {
+        if(CheckBuy(1,heroes[i]))
+        {setTimeout(HeroBuy,Random(300,600),10,heroes[i]);}
+        //todo: upgrades
+      }
+      else if(GetHeroLevel(heroes[i]) <= 15)
+      {
+        if(CheckBuy(10,heroes[i]))
+        {setTimeout(HeroBuy,Random(300,600),10,heroes[i]);}
+      }
+      else if(GetHeroLevel(heroes[i]) < 25)
+      {
+        if(CheckBuy(1,heroes[i]))
+        {setTimeout(HeroBuy,Random(300,600),1,heroes[i]);}
+      }
+      else
+      {
+        if(CheckBuy(10,heroes[i]))
+        {setTimeout(HeroUnlock,Random(300,600),heroes[i-1]);}
+      }
+    }
+  }
+  if(autoabil)
+  {
+    if(i == heroes.length) return;
+    for(j = i; j < heroes.length; j++)
+    {
+      var abil = heroes[j].childNodes[1];
+      if(abil.data == null)
+      {
+        if(abil.className.indexOf("disable") != -1) continue;
+        if(abil.className.indexOf("ready") == -1) continue;
+        abil.click();
+        abilscreen = 1;
+        return;
+      }
+    }
+  }
+}
   
+function CheckAbil(abil)
+{
+  var abilgold = GetAbilGold(abil);
+  return (abilgold < GetGold() && abilgold >= 0);
+}
+
+function AbilBuy(abil)
+{
+  abil.childNodes[3].click();
+}
+
+function GetAbilGold(abil)
+{
+  var abildata = abil.childNodes[3].childNodes[0].childNodes[0].data;
+  if(abildata.indexOf("Level") == -1)
+  {
+    return ParseGold(abil.childNodes[3].childNodes[0].childNodes[0].data);
+  }
+  return -1;
+}
+
+function CheckBuy(x, hero)
+{
+  SelectBuy(x);
+  return (GetHeroGold(hero) < GetGold());
+}
+  
+function WaitToUnlockHero(x, hero)
+{
+  while(1)
+  {
+    SelectBuy(x);
+    if(GetHeroGold(hero) < GetGold())
+    {
+      break;
+    }
+  }
+}
   
 function HeroUnlock(hero)
 {
   hero.childNodes[4].click();
 }
 
-function HeroBuy(x, hero)
+function WaitToBuyHero(x, hero)
+{
+  while(1)
+  {
+    SelectBuy(x);
+    if(GetHeroGold(hero) < GetGold())
+    {
+      break;
+    }
+  }
+}
+
+function SelectBuy(x)
 {
   var buyx = document.getElementsByClassName("all-x2-panel-box")[0].childNodes;
   switch(x)
@@ -25,12 +167,21 @@ function HeroBuy(x, hero)
       buyx[3].click();
       break;
   }
+}
+
+function HeroBuy(x, hero)
+{
+  SelectBuy(x);
   hero.childNodes[5].click();
 }
 
 function GetGold()
 {
-  var goldt = document.getElementsByClassName("res gold")[0].childNodes[0].childNodes[0].data;
+  return ParseGold(document.getElementsByClassName("res gold")[0].childNodes[0].childNodes[0].data);
+}
+
+function ParseGold(goldt)
+{
   var gold = 0.0;
   var i = 0;
   var div = 0;
@@ -75,6 +226,17 @@ function GetGold()
   }
   if(div > 0) gold /= div;
   return gold;
+}
+  
+function GetHeroGold(hero)
+{
+  var child = hero.childNodes;
+  return ParseGold(child[child.length-1].childNodes[0].childNodes[0].data);
+}
+  
+function GetHeroLevel(hero)
+{
+  return parseInt(hero.childNodes[4].childNodes[0].childNodes[0].data);
 }
 
 //document.getElementsByClassName("hero-card")[4].childNodes[5].click()
